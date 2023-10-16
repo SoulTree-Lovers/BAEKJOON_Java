846b8c69d220632e12141bcb3d6eeafb64a459e4
강승민 <rkdtmdals999@naver.com>
2023-10-16 11:24:29 +0900

    Update 2023.10.16

---
+++ Bronze_III/P_2441/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+// 별 찍기 - 4
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i=n; i>0; i--) {
+            System.out.print(" ".repeat(n-i));
+            System.out.println("*".repeat(i));
+        }
+
+
+    }
+}
